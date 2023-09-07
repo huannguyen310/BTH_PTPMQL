8d90f66cb11d872c35b02a73bbe674de619a0a3d
huannguyen310 <huanchappie0809@gmail.com>
2023-09-07 17:10:44 +0700

    Thuc hanh bien so

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection.Metadata;
+
+internal class Program
+{
+    private static void Main(String[] args)
+    {
+        int songuyen;
+        float sothuc;
+        string chuoi;
+        char kytu;
+        bool dungsai;
+        songuyen = 10;
+        sothuc = 5.5F;
+        chuoi = "chuoi";
+        kytu = 'h';
+        dungsai = true;
+        Console.WriteLine("Kieu so nguyen: {0}",songuyen);
+        Console.WriteLine("Kieu so thuc: {0}",sothuc);
+        Console.WriteLine("Kieu chuoi:{0}",chuoi);
+        Console.WriteLine("Kieu ky tu: {0}",kytu);
+        Console.WriteLine("Kieu bool: {0}",dungsai);
+    }
+}
