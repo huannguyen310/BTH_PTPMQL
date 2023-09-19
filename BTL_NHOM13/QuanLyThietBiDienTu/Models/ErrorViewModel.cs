9bc38dec15112e33d70f48421432dd754d4759b5
huannguyen310 <huanchappie0809@gmail.com>
2023-09-19 12:03:44 +0700

    del file

--- BTL_NHOM13/QuanLyThietBiDienTu/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace QuanLyThietBiDienTu.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
