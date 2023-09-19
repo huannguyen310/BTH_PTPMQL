6bcff29621db98befe0f8cdce23b78145ddc3f97
huannguyen310 <huanchappie0809@gmail.com>
2023-09-19 11:54:50 +0700

    BTL_Nhom13

---
+++ BTL_NHOM13/QuanLyThietBiDienTu/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace QuanLyThietBiDienTu.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
