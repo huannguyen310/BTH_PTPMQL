1d217c7938ceef2335d6b55a93add719694c1759
huannguyen310 <huanchappie0809@gmail.com>
2023-10-21 17:07:13 +0700

    Create controller

---
+++ Project/FirtsWebMVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+namespace FirtsWebMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
