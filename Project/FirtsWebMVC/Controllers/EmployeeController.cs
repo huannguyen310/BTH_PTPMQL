76ba1e435e0954a19ceeb6ef521416efd1c776b3
huannguyen310 <huanchappie0809@gmail.com>
2023-10-26 16:53:12 +0700

    Gửi dữ liệu đối tượng Employee từ View lên Controller

--- Project/FirtsWebMVC/Controllers/EmployeeController.cs
+++ Project/FirtsWebMVC/Controllers/EmployeeController.cs
@@ -1,3 +1,4 @@
+using FirtsWebMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 namespace FirtsWebMVC.Controllers
 {
@@ -5,6 +6,13 @@ namespace FirtsWebMVC.Controllers
     {
         public IActionResult Action1()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Action1(Employee emp)
+        {
+            string empOutput = "ID: " + emp.EmployeeId + " - Name: " + emp.FullName.ToUpper() + " - Dia chi: " + " - Age: " + emp.Age;
+            ViewBag.thongbao = empOutput;
             return View();
         }
         public IActionResult Action2()
