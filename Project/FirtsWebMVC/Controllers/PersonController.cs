cf69134d47e0bcf8d36b9d390b49c83d8a566d9a
huannguyen310 <huanchappie0809@gmail.com>
2023-12-07 17:36:44 +0700

    Lam viec voi file excel

--- Project/FirtsWebMVC/Controllers/PersonController.cs
+++ Project/FirtsWebMVC/Controllers/PersonController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using FirtsWebMVC.Data;
 using FirtsWebMVC.Models;
+using FirtsWebMVC.Models.Process;
+using OfficeOpenXml;
 
 namespace FirtsWebMVC.Controllers
 {
@@ -12,6 +14,7 @@ namespace FirtsWebMVC.Controllers
         {
             _context = context;
         }
+        private ExcelProcess _excelPro = new ExcelProcess();
         public async Task<IActionResult> Index()
         {
             var model = await _context.Person.ToListAsync();
@@ -123,6 +126,67 @@ namespace FirtsWebMVC.Controllers
         private bool PersonExist(string id)
         {
             return (_context.Person?.Any(e => e.PersonID == id)).GetValueOrDefault();
+        }
+        public async Task<IActionResult> Upload()
+        {
+            return View();   
+        }
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file!=null)
+                {
+                    string fileExtension = Path.GetExtension(file.FileName);
+                    if (fileExtension != ".xls" && fileExtension != ".xlsx")
+                    {
+                        ModelState.AddModelError("", "Please choose excel file to upload!");
+                    }
+                    else
+                    {
+                        //rename file when upload to server
+                        var filePath = Path.Combine(Directory.GetCurrentDirectory() + "/Uploads/Excels", "File" + DateTime.Now.Day + DateTime.Now.Hour + DateTime.Now.Minute + DateTime.Now.Millisecond + fileExtension);
+                        var fileLocation = new FileInfo(filePath).ToString();
+                        if (file.Length > 0)
+                        {
+                            using (var stream = new FileStream(filePath, FileMode.Create))
+                            {
+                                //save file to server
+                                await file.CopyToAsync(stream);
+                                //read data from file and write to database
+                                var dt = _excelPro.ExcelToDataTable(fileLocation);
+                                for(int i = 0; i < dt.Rows.Count; i++)
+                                {
+                                    var ps = new Person();
+                                    ps.PersonID = dt.Rows[i][0].ToString();
+                                    ps.FullName = dt.Rows[i][1].ToString();
+                                    ps.Address = dt.Rows[i][2].ToString();
+                                    ps.Gender = dt.Rows[i][3].ToString();
+                                    _context.Add(ps);
+                                }
+                                await _context.SaveChangesAsync();
+                                return RedirectToAction(nameof(Index));
+                            }
+                        }
+                    }
+                }
+            
+            return View();
+        }
+        public IActionResult Download()
+        {
+            var fileName = "YourFileName.xlsx";
+            using(ExcelPackage excelPackage = new ExcelPackage())
+            {
+                ExcelWorksheet excelWorksheet = excelPackage.Workbook.Worksheets.Add("Sheet 1");
+                excelWorksheet.Cells["A1"].Value = "PersonID";
+                excelWorksheet.Cells["B1"].Value = "FullName";
+                excelWorksheet.Cells["C1"].Value = "Address";
+                excelWorksheet.Cells["D1"].Value = "Gender";
+                var pslist = _context.Person.ToList();
+                excelWorksheet.Cells["A2"].LoadFromCollection(pslist);
+                var stream = new MemoryStream(excelPackage.GetAsByteArray());
+                return File(stream,"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",fileName);
+            }
         }
     }
 }
